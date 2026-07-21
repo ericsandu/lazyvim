f113978668a7d0cfd214041c5caaab21b1e933fb
Eric Sandu <eric_andrei.sandu@stud.acs.upb.ro>
2026-07-21 15:12:40 +0300

    feat(preview): add preview shortcut on supported filetypes

--- lua/plugins/livepreview.lua
+++ lua/plugins/livepreview.lua
@@ -4,4 +4,7 @@ return {
     -- You can choose one of the following pickers
     "folke/snacks.nvim",
   },
+  keys = {
+    { "<leader>cp", "<cmd>LivePreview start<cr>", desc = "Live Preview", ft = { "markdown", "html", "css", "javascript" } },
+  },
 }
