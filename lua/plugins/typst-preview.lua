5264e6b116429a997f0e9d7eb400540d98af8137
Eric Sandu <eric_andrei.sandu@stud.acs.upb.ro>
2026-09-06 20:45:14 +0200

    feat(lazyvim): enable typst.lang

---
+++ lua/plugins/typst-preview.lua
@@ -0,0 +1,9 @@
+return {
+  "chomosuke/typst-preview.nvim",
+  opts = {
+    dependencies_bin = {
+      ["tinymist"] = "/usr/bin/tinymist",
+      ["websocat"] = "/usr/bin/websocat",
+    },
+  },
+}
